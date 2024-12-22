fc1b3e2101a0cd491f65abb3b2f604c9643f53c5
CNOVLISme-N <nodirbekhamidullayev10@gmail.com>
2024-12-22 21:58:28 +0500

    Tug'ulgan kuning bilan !

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function displayGreeting() {
   const birthdayMessage = document.getElementById('birthdayMessage');
 
   if (name) {
-    birthdayMessage.innerHTML = `${name} - Seni tug'ulgan kuning bilan chin qalbimdan tabriklayman ! Ortda qolgan yoshingda , qilgan barcha yaxshi orzu va maqsadlaringni amalga oshira olmagan bo'lsang , shu yoshingda amalga oshirishing tilakdoshiman . Bugun seni eng quvonchli va unitilmas kuning . Senga baxt , sog'lik salomatlik va omad tilab qolaman . Hayotingda doimo faqat quvonchli onlar bo‘lsin , har bir kuning baxtga to‘lsin . Ota - onang sen bilan faxrlanadigon farzand bo'lib ulg'aygin . O'qishlaringga omad va ishlaringga rivoj . `;
+    birthdayMessage.innerHTML = `${name} - Seni tug'ulgan kuning bilan chin qalbimdan tabriklayman ! Ortda qolgan yoshingda , qilgan barcha yaxshi maqsadlaringni amalga oshira olmagan bo'lsang , shu yoshingda amalga oshirishing tilakdoshiman . Bugun seni eng quvonchli va unitilmas kuning . Senga baxt , sog'lik salomatlik va omad tilab qolaman . Hayotingda doimo faqat quvonchli onlar bo‘lsin , har bir kuning baxtga to‘lsin . Ota - onang sen bilan faxrlanadigon farzand bo'lib ulg'aygin . O'qishlaringga omad va ishlaringga rivoj . `;
   } else {
     birthdayMessage.innerHTML = 'Tug\'ulgan Kuningiz Muborak!';
   }
